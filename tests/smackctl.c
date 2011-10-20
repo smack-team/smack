16ba18c85d3cbd41440d391b2de1509e3b3bb1be
Jarkko Sakkinen <jarkko.sakkinen@intel.com>
2011-10-20 11:47:35 +0300

    smackctl: applying rules from /etc/smack/accesses.d

--- tests/smackctl.c
+++ tests/smackctl.c
@@ -21,6 +21,8 @@
  * Jarkko Sakkinen <jarkko.sakkinen@intel.com>
  */
 
+#include <alloca.h>
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
@@ -34,6 +36,8 @@
 #include <unistd.h>
 
 #define SMACKFS_MAGIC 0x43415d53
+#define ACCESSES_D_PATH "/etc/smack/accesses.d"
+#define PATH_SIZE 1000
 
 static int apply(void);
 static int clear(void);
@@ -70,20 +74,73 @@ int main(int argc, char **argv)
 static int apply(void)
 {
 	struct stat sbuf;
+	DIR *dir;
+	struct dirent *ent;
+	char pathbuf[PATH_SIZE];
 
 	if (is_smackfs_mounted() != 1) {
 		fprintf(stderr, "ERROR: SmackFS is not mounted.\n");
 		return -1;
 	}
 
-	if (apply_rules("/smack/load", SMACK_RULE_SET_APPLY_CLEAR))
+	if (clear())
 		return -1;
 
 	if (stat("/etc/smack/accesses", &sbuf) && errno != ENOENT) {
 		perror("stat");
+		clear();
 		return -1;
-	} else if (errno == 0 && apply_rules("/etc/smack/accesses", 0))
+	} else if (errno == 0 && apply_rules("/etc/smack/accesses", 0)) {
+		clear();
 		return -1;
+	}
+
+	if (stat(ACCESSES_D_PATH, &sbuf) && errno != ENOENT) {
+		perror("stat");
+		clear();
+		return -1;
+	} else if (errno == 0) {
+		errno = 0;
+		dir = opendir(ACCESSES_D_PATH);
+		if (dir == NULL) {
+			perror("opendir");
+			clear();
+			return -1;
+		}
+
+		for (;;) {
+			errno = 0;
+			ent = readdir(dir);
+			if (ent == NULL) {
+				closedir(dir);
+				if (errno) {
+					perror("readdir");
+					clear();
+					return -1;
+				}
+				break;
+			}
+
+			snprintf(pathbuf, PATH_SIZE, "%s/%s", ACCESSES_D_PATH,
+				 ent->d_name);
+
+			if (stat(pathbuf, &sbuf)) {
+				perror("stat");
+				closedir(dir);
+				clear();
+				return -1;
+			}
+
+			if (S_ISDIR(sbuf.st_mode))
+				continue;
+
+			if (apply_rules(pathbuf, 0)) {
+				closedir(dir);
+				clear();
+				return -1;
+			}
+		}
+	}
 
 	return 0;
 }
