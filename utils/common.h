3fee29a4d362b6e4b0d4185ea3c9660b3ca92b71
Jarkko Sakkinen <jarkko.sakkinen@intel.com>
2013-06-18 09:37:42 +0300

    Added AUTHORS file.

--- utils/common.h
+++ utils/common.h
@@ -16,10 +16,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA
- *
- * Authors:
- * Brian McGillion <brian.mcgillion@intel.com>
- * Jarkko Sakkinen <jarkko.sakkinen@intel.com>
  */
 
 #ifndef COMMON_H
--- utils/smackcipso.c
+++ utils/smackcipso.c
@@ -16,9 +16,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA
- *
- * Authors:
- * Brian McGillion <brian.mcgillion@intel.com>
  */
 
 #include "common.h"
