smack-team/smack

commit ce452ab29796dbe945b0fa1b400c9948d837ddc7

Removed redundant is_smackfs_mounted().
This functionality is fully handled in the library initialization.

utils/smackctl.c
@@ -19,14 +19,15 @@
  */
 
 #include "common.h"
+#include <sys/smack.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 
 static int apply_all(void)
 {
-	if (is_smackfs_mounted() != 1) {
-		fprintf(stderr, "ERROR: SmackFS is not mounted.\n");
+	if (!smack_smackfs_path()) {
+		fprintf(stderr, "SmackFS is not mounted.\n");
 		return -1;
 	}
 
@@ -42,24 +43,9 @@ static int apply_all(void)
 	return 0;
 }
 
-static int status(void)
-{
-	int ret = is_smackfs_mounted();
-
-	switch (ret) {
-	case 1:
-		printf("SmackFS is mounted.\n");
-		return 0;
-	case 0:
-		printf("SmackFS is not mounted.\n");
-		return 0;
-	default:
-		return -1;
-	}
-}
-
 int main(int argc, char **argv)
 {
+	const char *tmp = smack_smackfs_path();
 	if (argc < 2) {
 		fprintf(stderr, "Usage: %s <action>\n", argv[0]);
 		return 1;
@@ -72,12 +58,14 @@ int main(int argc, char **argv)
 		if (clear())
 			return 1;
 	} else if (!strcmp(argv[1], "status")) {
-		if (status())
-			return 1;
-	} else {
-		fprintf(stderr, "Uknown action: %s\n", argv[1]);
-		return 1;
+		if (smack_smackfs_path())
+			printf("SmackFS is mounted to %s.\n",
+			       smack_smackfs_path());
+		else
+			printf("SmackFS is not mounted.\n");
+		return 0;
 	}
 
-	return 0;
+	fprintf(stderr, "Uknown action: %s\n", argv[1]);
+	return 1;
 }

utils/common.c
@@ -37,31 +37,6 @@
 static int apply_rules_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf);
 static int apply_cipso_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf);
 
-int is_smackfs_mounted(void)
-{
-	struct statfs sfs;
-	int ret;
-	const char * smack_mnt;
-
-	smack_mnt = smack_smackfs_path();
-	if (!smack_mnt) {
-		errno = EFAULT;
-		return -1;
-	}
-
-	do {
-		ret = statfs(smack_mnt, &sfs);
-	} while (ret < 0 && errno == EINTR);
-
-	if (ret)
-		return -1;
-
-	if (sfs.f_type == SMACK_MAGIC)
-		return 1;
-
-	return 0;
-}
-
 int clear(void)
 {
 	int fd;
@@ -75,7 +50,7 @@ int clear(void)
 		return -1;
 	}
 
-	if (is_smackfs_mounted() != 1)
+	if (!smack_smackfs_path())
 		return -1;
 
 	snprintf(path, sizeof path, "%s/load2", smack_mnt);

utils/common.h
@@ -25,7 +25,6 @@
 #define CIPSO_D_PATH "/etc/smack/cipso.d"
 
 int clear(void);
-int is_smackfs_mounted(void);
 int apply_rules(const char *path, int clear);
 int apply_cipso(const char *path);
 int apply_rules_file(int fd, int clear);

utils/smackcipso.c
@@ -25,7 +25,7 @@
 
 int main(int argc, char **argv)
 {
-	if (is_smackfs_mounted() != 1) {
+	if (!smack_smackfs_path()) {
 		fprintf(stderr, "SmackFS is not mounted.\n");
 		exit(1);
 	}

utils/smackload.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 	int clear = 0;
 	int c;
 
-	if (is_smackfs_mounted() != 1) {
+	if (!smack_smackfs_path()) {
 		fprintf(stderr, "SmackFS is not mounted.\n");
 		exit(1);
 	}

commit 3d73d2d2db601f05896dafe372f41b4995b12cec

smackctl: fix reporting "unknown action" (regression in ce452ab)
Running "smackctl apply" and "smackctl clear" ended with error and
message about unknown action, although the action was known and
performed successfully.

Signed-off-by: Rafal Krypa <r.krypa@samsung.com>

utils/smackctl.c
@@ -65,8 +65,10 @@ int main(int argc, char **argv)
 		else
 			printf("SmackFS is not mounted.\n");
 		exit(0);
+	} else {
+		fprintf(stderr, "Uknown action: %s\n", argv[1]);
+		exit(1);
 	}
 
-	fprintf(stderr, "Uknown action: %s\n", argv[1]);
-	exit(1);
+	exit(0);
 }