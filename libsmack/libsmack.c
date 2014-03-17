smack-team/smack

commit d7319c71d9227cf5daa8042d442e1bf6846f3e4c

libsmack: add a common function for opening long and short label file
Added internal function 'open_smackfs_file()' to open long label
file and as a fallback short label file. This can be used for
load, access and cipso files.

Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>

libsmack/libsmack.c
@@ -98,6 +98,8 @@ struct smack_cipso {
 	struct cipso_mapping *last;
 };
 
+static int open_smackfs_file(const char *long_name, const char *short_name,
+			     int *use_long);
 static int accesses_apply(struct smack_accesses *handle, int clear);
 static int accesses_print(struct smack_accesses *handle, int clear,
 			  int load_fd, int change_fd, int use_long, int add_lf);
@@ -280,27 +282,20 @@ int smack_have_access(const char *subject, const char *object,
 	int code;
 	int ret;
 	int fd;
-	int access2 = 1;
+	int use_long = 1;
 
 	if (smackfs_mnt_dirfd < 0)
 		return -1;
 
-	fd = openat(smackfs_mnt_dirfd, "access2", O_RDWR);
-	if (fd < 0) {
-		if (errno != ENOENT)
-			return -1;
-		
-		fd = openat(smackfs_mnt_dirfd, "access", O_RDWR);
-		if (fd < 0)
-			return -1;
-		access2 = 0;
-	}
+	fd = open_smackfs_file("access2", "access", &use_long);
+	if (fd < 0)
+		return -1;
 
 	if ((code = str_to_access_code(access_type)) < 0)
 		return -1;
 	access_code_to_str(code, str);
 
-	if (access2)
+	if (use_long)
 		ret = snprintf(buf, LOAD_LEN + 1, KERNEL_LONG_FORMAT,
 			       subject, object, str);
 	else
@@ -600,26 +595,41 @@ int smack_revoke_subject(const char *subject)
 	return (ret < 0) ? -1 : 0;
 }
 
+static int open_smackfs_file(const char *long_name, const char *short_name,
+			     int *use_long)
+{
+	int fd;
+
+	fd = openat(smackfs_mnt_dirfd, long_name, O_WRONLY);
+	if (fd < 0) {
+		if (errno != ENOENT)
+			return -1;
+
+		fd = openat(smackfs_mnt_dirfd, short_name, O_WRONLY);
+		if (fd < 0)
+			return -1;
+
+		*use_long = 0;
+		return fd;
+	}
+
+	*use_long = 1;
+	return fd;
+}
+
 static int accesses_apply(struct smack_accesses *handle, int clear)
 {
 	int ret;
 	int load_fd;
 	int change_fd;
-	int load2 = 1;
+	int use_long = 1;
 
 	if (smackfs_mnt_dirfd < 0)
 		return -1;
 
-	load_fd = openat(smackfs_mnt_dirfd, "load2", O_WRONLY);
-	if (load_fd < 0) {
-		if (errno != ENOENT)
-			return -1;
-		/* fallback */
-		load_fd = openat(smackfs_mnt_dirfd, "load", O_WRONLY);
-		if (load_fd < 0)
-			return -1;
-		load2 = 0;
-	}
+	load_fd = open_smackfs_file("load2", "load", &use_long);
+	if (load_fd < 0)
+		return -1;
 
 	change_fd = openat(smackfs_mnt_dirfd, "change-rule", O_WRONLY);
 	/* Try to continue if the file doesn't exist, we might not need it. */
@@ -628,7 +638,7 @@ static int accesses_apply(struct smack_accesses *handle, int clear)
 		goto err_out;
 	}
 
-	ret = accesses_print(handle, clear, load_fd, change_fd, load2, 0);
+	ret = accesses_print(handle, clear, load_fd, change_fd, use_long, 0);
 
 err_out:
 	if (load_fd >= 0)

commit a69b89b57d854b4db7aee657c3e7de3b56f24832

libsmack: fix smack_have_access() (regression in d7319c71)
Commit d7319c71 introduced an internal function for opening smackfs files,
when there is a long and short label version. The new function always
opens the file write only, but smack_have access() requires O_RDWR.
The internal function is now extended to take argument with file access
mode.

Signed-off-by: Rafal Krypa <r.krypa@samsung.com>

libsmack/libsmack.c
@@ -128,7 +128,7 @@ struct smack_file_buffer {
 };
 
 static int open_smackfs_file(const char *long_name, const char *short_name,
-			     int *use_long);
+			     mode_t mode, int *use_long);
 static int accesses_apply(struct smack_accesses *handle, int clear);
 static int accesses_print(struct smack_accesses *handle,
 			  int clear, int use_long, int multiline,
@@ -365,7 +365,7 @@ int smack_have_access(const char *subject, const char *object,
 	if (slen < 0 || olen < 0)
 		return -1;
 
-	fd = open_smackfs_file("access2", "access", &use_long);
+	fd = open_smackfs_file("access2", "access", O_RDWR, &use_long);
 	if (fd < 0)
 		return -1;
 
@@ -445,7 +445,7 @@ int smack_cipso_apply(struct smack_cipso *cipso)
 	if (init_smackfs_mnt())
 		return -1;
 
-	fd = open_smackfs_file("cipso2", "cipso", &use_long);
+	fd = open_smackfs_file("cipso2", "cipso", O_WRONLY, &use_long);
 	if (fd < 0)
 		return -1;
 
@@ -690,16 +690,16 @@ int smack_revoke_subject(const char *subject)
 }
 
 static int open_smackfs_file(const char *long_name, const char *short_name,
-			     int *use_long)
+			     mode_t mode, int *use_long)
 {
 	int fd;
 
-	fd = openat(smackfs_mnt_dirfd, long_name, O_WRONLY);
+	fd = openat(smackfs_mnt_dirfd, long_name, mode);
 	if (fd < 0) {
 		if (errno != ENOENT)
 			return -1;
 
-		fd = openat(smackfs_mnt_dirfd, short_name, O_WRONLY);
+		fd = openat(smackfs_mnt_dirfd, short_name, mode);
 		if (fd < 0)
 			return -1;
 
@@ -740,7 +740,7 @@ static int accesses_apply(struct smack_accesses *handle, int clear)
 	if (init_smackfs_mnt())
 		return -1;
 
-	load_buffer.fd = open_smackfs_file("load2", "load", &use_long);
+	load_buffer.fd = open_smackfs_file("load2", "load", O_WRONLY, &use_long);
 	if (load_buffer.fd < 0)
 		return -1;
 	load_buffer.buf = malloc(handle->page_size + LOAD_LEN);