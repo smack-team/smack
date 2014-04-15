e8ae32cec19b0fa12a6c6845b5bd569d76c6c855
José Bollo <jose.bollo@open.eurogiciel.org>
2014-04-15 09:12:45 +0200

    smackctl: Adding reliable usage
    Adding help text providing usage information for smackctl utility.
    Minor improvement of the manual page for smackctl.
    
    Change-Id: I31f8fd4a4c866284255e4865bf7bb9512e3c793f
    Signed-off-by: José Bollo <jose.bollo@open.eurogiciel.org>

--- utils/smackctl.c
+++ utils/smackctl.c
@@ -29,19 +29,24 @@
 #include <libgen.h>
 #include "config.h"
 
-static const char usage[] =
-	"Usage: %s [options] <subject> <object> <access>\n"
+static const char usage[] = 
+	"Usage: %s [option] [action]\n"
 	"options:\n"
 	" -v --version       output version information and exit\n"
 	" -h --help          output usage information and exit\n"
+	"actions:\n"
+	" apply   apply all the rules found in the configuration directory's\n"
+	" clear   remove all system rules from the kernel\n"
+	" status  show the status of the Smack system, specifically if "
+	       "smackfs is mounted\n"
 ;
 
 static const char short_options[] = "vh";
 
 static struct option options[] = {
-	{"version", no_argument, 0, 'v'},
-	{"help", no_argument, 0, 'h'},
-	{NULL, 0, 0, 0}
+	{"version", no_argument, NULL, 'v'},
+	{"help", no_argument, NULL, 'h'},
+	{NULL, 0, NULL, 0}
 };
 
 static int apply_all(void)
@@ -65,7 +70,6 @@ static int apply_all(void)
 
 int main(int argc, char **argv)
 {
-	const char *cmd;
 	int c;
 
 	for ( ; ; ) {
@@ -98,20 +102,22 @@ int main(int argc, char **argv)
 	if (!strcmp(cmd, "apply")) {
 		if (apply_all())
 			exit(1);
-	} else if (!strcmp(cmd, "clear")) {
+	} else if (!strcmp(argv[1], "clear")) {
 		if (clear())
 			exit(1);
-	} else if (!strcmp(cmd, "status")) {
+	} else if (!strcmp(argv[1], "status")) {
 		if (smack_smackfs_path())
-			printf("SmackFS is mounted to %s.\n",
+			printf("SmackFS is mounted to %s\n",
 			       smack_smackfs_path());
 		else
 			printf("SmackFS is not mounted.\n");
 		exit(0);
 	} else {
 		fprintf(stderr, "Uknown action: %s\n", argv[1]);
+		fprintf(stderr, usage, argv[0]);
 		exit(1);
 	}
 
 	exit(0);
+	return 0;
 }
