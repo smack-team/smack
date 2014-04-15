smack-team/smack

commit e68907525666ce95a66976df89ebf0ee2a3d22b8

libsmack: fix label validation in smack_new_label_from_path
Off-by-one length was returned from the function. This patch fixes
the regression by doing full validation for the SMACK label.

Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>

libsmack/libsmack.c
@@ -581,22 +581,21 @@ ssize_t smack_new_label_from_socket(int fd, char **label)
 ssize_t smack_new_label_from_path(const char *path, const char *xattr, 
 				  int follow, char **label)
 {
+	char buf[SMACK_LABEL_LEN + 1];
 	char *result;
 	ssize_t ret = 0;
 
 	ret = follow ?
-		getxattr(path, xattr, NULL, 0) :
-		lgetxattr(path, xattr, NULL, 0);
-	if (ret < 0 && errno != ERANGE)
+		getxattr(path, xattr, buf, SMACK_LABEL_LEN + 1) :
+		lgetxattr(path, xattr, buf, SMACK_LABEL_LEN + 1);
+	if (ret < 0)
 		return -1;
 
 	result = calloc(ret + 1, 1);
 	if (result == NULL)
 		return -1;
 
-	ret = follow ?
-		getxattr(path, xattr, result, ret) :
-		lgetxattr(path, xattr, result, ret);
+	ret = get_label(result, buf);
 	if (ret < 0) {
 		free(result);
 		return -1;

commit d26f2fafb2a5c4d2c8ae613a971e93c4fec2675f

libsmack: fix smack_new_label_from_path() (regression in e6890752)
Function smack_new_label_from_path failed to null-terminate xattr value
before passing it to get_label.

Signed-off-by: Rafal Krypa <r.krypa@samsung.com>

libsmack/libsmack.c
@@ -631,6 +631,7 @@ ssize_t smack_new_label_from_path(const char *path, const char *xattr,
 		lgetxattr(path, xattr, buf, SMACK_LABEL_LEN + 1);
 	if (ret < 0)
 		return -1;
+	buf[ret] = '\0';
 
 	result = calloc(ret + 1, 1);
 	if (result == NULL)